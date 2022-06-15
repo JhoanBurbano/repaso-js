c6ad5c63889c25bb2eff21eefe6ae66f95904aff
Cibaax <sebasfer@outlook.com>
2022-06-15 10:58:04 -0500

    comppletar funcion

--- aldair/ejercicios.js
+++ aldair/ejercicios.js
@@ -1,3 +1,4 @@
+const { thisExpression } = require('@babel/types');
 const { Linkedlist } = require('../linkedkist.js')
 
 require('../linkedkist.js')
@@ -5,6 +6,13 @@ require('../linkedkist.js')
 //implementar la funcion doblar valor de cada nodo de una lista enlazada
 
 Linkedlist.prototype.doblarValor = function(){
+    if(this.head === null){
+        return 0
+    }
+    if(this.head.next === null){
+        this.head.value = this.head.value * 2
+        return
+    }
     let current = this.head;
     while(current !== null){
         current.value = current.value * 2;
