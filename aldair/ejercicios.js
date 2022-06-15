20f5b608d9bac1ab7df63931844c84c4b9d31372
Cibaax <sebasfer@outlook.com>
2022-06-15 09:01:55 -0500

    linkedlist excersice

--- aldair/ejercicios.js
+++ aldair/ejercicios.js
@@ -1,2 +1,7 @@
 require('../linkedkist.js')
 
+//implementar la funcion doblar valor de cada nodo de una lista enlazada
+
+Linkedlist.prototype.doblarValor = function(){
+    
+}
---
+++ jhon/ejercicios.js
@@ -0,0 +1,6 @@
+require('../linkedkist.js')
+
+//implementar la funcion que reciba un numero como parametro y retorne el valor del nodo que se encuentra en la posicion pasada dentro de la lista enlazada
+Linkedlist.prototype.getNodo = function(posicion){
+    
+}
